4592a225a6482abedf426eeafbf60ecc1032d51a
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-11-13 15:58:39 -0300

    Initial commit

---
+++ src/02-insertProducts.ts
@@ -0,0 +1,3 @@
+// export default function insertProducts() {
+//   // insira a solução
+// }
---
+++ src/03-findById.ts
@@ -0,0 +1,27 @@
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Camiseta',
+//     price: 29.9,
+//     quantity: 3,
+//     brands: ['Nike', 'Reebok'],
+//   },
+//   {
+//     id: 2,
+//     name: 'Caneca',
+//     price: 19.90,
+//     quantity: 12,
+//     brands: ['Stanley', 'Camelbak'],
+//   },
+//   {
+//     id: 3,
+//     name: 'Boné',
+//     price: 39.90,
+//     quantity: 0,
+//     brands: [],
+//   },
+// ];
+
+// export default function findById() {
+//   // insira a solução
+// }
