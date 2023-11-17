cbaebb9827d33130a087d96b1e4b1abcb579766b
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-17 15:19:54 -0300

    Requisito 02 - Implemente a função insertProducts

--- src/02-insertProducts.ts
+++ src/02-insertProducts.ts
@@ -1,3 +1,14 @@
-// export default function insertProducts() {
-//   // insira a solução
-// }
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  quantity: number
+  brands: string[]
+};
+
+export default function insertProducts(availableBrands: string[], product: Product): string {
+  if (product.brands.every((brand) => availableBrands.includes(brand))) {
+    return `${product.name} adicionado(a) com o preço de R$ ${product.price}`;
+  }
+  return 'Seu produto contém marcas indisponíveis.';
+}
