d56209041ce656f151e64e4bde1af469180cd66b
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-17 13:50:13 -0300

    Requisito 01 - Implemente a função insert

--- src/01-insert.ts
+++ src/01-insert.ts
@@ -1,3 +1,3 @@
-// export default function insert() {
-//   // insira a solução
-// }
+export default function insert(productName: string): string {
+  return `${productName} adicionado(a) com sucesso!`;
+}
