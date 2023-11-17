23ec5c226841b152402453581ede4c601ee95fa7
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-17 19:19:53 -0300

    Requisito 03 - Implemente a função findById

--- src/03-findById.ts
+++ src/03-findById.ts
@@ -1,27 +1,33 @@
-// const products = [
-//   {
-//     id: 1,
-//     name: 'Camiseta',
-//     price: 29.9,
-//     quantity: 3,
-//     brands: ['Nike', 'Reebok'],
-//   },
-//   {
-//     id: 2,
-//     name: 'Caneca',
-//     price: 19.90,
-//     quantity: 12,
-//     brands: ['Stanley', 'Camelbak'],
-//   },
-//   {
-//     id: 3,
-//     name: 'Boné',
-//     price: 39.90,
-//     quantity: 0,
-//     brands: [],
-//   },
-// ];
+import { Product } from './02-insertProducts';
 
-// export default function findById() {
-//   // insira a solução
-// }
+const products: Product[] = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: 29.9,
+    quantity: 3,
+    brands: ['Nike', 'Reebok'],
+  },
+  {
+    id: 2,
+    name: 'Caneca',
+    price: 19.90,
+    quantity: 12,
+    brands: ['Stanley', 'Camelbak'],
+  },
+  {
+    id: 3,
+    name: 'Boné',
+    price: 39.90,
+    quantity: 0,
+    brands: [],
+  },
+];
+
+export default function findById(productId: number): Product | string {
+  const product = products.find((myProduct) => myProduct.id === productId);
+  if (product) {
+    return product;
+  }
+  return `Produto com ID ${productId} não encontrado.`;
+}
